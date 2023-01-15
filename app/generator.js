379b561001e83040942796176c0ea5af58fa69cc
Alex Toledo <dadalasalsa@gmail.com>
2023-01-15 14:31:18 -0500

    OS updates

--- app/generator.js
+++ app/generator.js
@@ -20,26 +20,26 @@ class Generator {
 
     createSwapFunctions(path) {
         tools.forEach((tool) => {
-            creator.write(`${path}/${tool.file}.mcfunction`, swap.create('item', config, tool));
-            creator.write(`${path}/${tool.file}_gate.mcfunction`, swap.create('item_gate', config, tool));
+            creator.write(`${path}${process.env.DELIMETER}${tool.file}.mcfunction`, swap.create('item', config, tool));
+            creator.write(`${path}${process.env.DELIMETER}${tool.file}_gate.mcfunction`, swap.create('item_gate', config, tool));
         });
     }
     createBookFunctions(path) {
-        creator.write(`${path}/god_book.mcfunction`, book.create('god'));
-        creator.write(`${path}/default_book.mcfunction`, book.create('default'));
+        creator.write(`${path}${process.env.DELIMETER}book_god.mcfunction`, book.create('god'));
+        creator.write(`${path}${process.env.DELIMETER}book_default.mcfunction`, book.create('default'));
     }
     createInventoryFunctions(path) {
-        creator.write(`${path}/inventory_import.mcfunction`, inventory.create('import', config));
-        creator.write(`${path}/inventory_import_gate.mcfunction`, inventory.create('import_gate', config));
-        creator.write(`${path}/inventory_export.mcfunction`, inventory.create('export', config, items));
-        creator.write(`${path}/inventory_export_gate.mcfunction`, inventory.create('export_gate', config));
+        creator.write(`${path}${process.env.DELIMETER}inventory_import.mcfunction`, inventory.create('import', config));
+        creator.write(`${path}${process.env.DELIMETER}inventory_import_gate.mcfunction`, inventory.create('import_gate', config));
+        creator.write(`${path}${process.env.DELIMETER}inventory_export.mcfunction`, inventory.create('export', config, items));
+        creator.write(`${path}${process.env.DELIMETER}inventory_export_gate.mcfunction`, inventory.create('export_gate', config));
     }
     createEnderFunctions(path) {
-        creator.write(`${path}/ender_place.mcfunction`, enderchest.create('place', config, directions));
-        creator.write(`${path}/ender_destroy.mcfunction`, enderchest.create('destroy', config, directions));
-        creator.write(`${path}/ender_inventory_gate.mcfunction`, enderchest.create('inventory_gate', config, directions));
-        creator.write(`${path}/ender_existence_gate.mcfunction`, enderchest.create('existence_gate', config, directions));
-        creator.write(`${path}/ender_toggle.mcfunction`, enderchest.create('toggle', config, directions));
+        creator.write(`${path}${process.env.DELIMETER}ender_place.mcfunction`, enderchest.create('place', config, directions));
+        creator.write(`${path}${process.env.DELIMETER}ender_destroy.mcfunction`, enderchest.create('destroy', config, directions));
+        creator.write(`${path}${process.env.DELIMETER}ender_inventory_gate.mcfunction`, enderchest.create('inventory_gate', config, directions));
+        creator.write(`${path}${process.env.DELIMETER}ender_existence_gate.mcfunction`, enderchest.create('existence_gate', config, directions));
+        creator.write(`${path}${process.env.DELIMETER}ender_toggle.mcfunction`, enderchest.create('toggle', config, directions));
     }
 }
 
