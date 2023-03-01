75c189d0a9a896d6104cbde4f11060f9b1b4d930
Alex Toledo <dadalasalsa@gmail.com>
2023-03-01 09:41:34 -0500

    Updating locations and adding totem tracking

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
 require('dotenv').config();
 const path = require('path');
 const generator = require('./app/generator');
+const resource = require('./app/resource');
 
 generator.init(path.resolve(process.env.BASE_PATH, process.env.PACK_PATH));
 generator.create();
+
+console.log(resource.create(require('./data/resources.json')));
